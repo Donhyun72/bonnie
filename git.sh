882146f224d55de49baceee07f666036748a7ae1
donhyun-lee <donhyun.lee@pubg.com>
2020-11-17 09:32:01 +0000

    first commit

---
+++ git.sh
@@ -0,0 +1,14 @@
+# New repo
+echo "# bonnie" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Donhyun72/bonnie.git
+git push -u origin main
+                
+
+# push an existing repo
+git remote add origin https://github.com/Donhyun72/bonnie.git
+git branch -M main
+git push -u origin main
